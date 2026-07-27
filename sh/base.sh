866c84762d799989d902c5ab3661de385c7a9f0e
yunlk <1357602588@qq.com>
2026-07-27 10:24:52 +0800

    bash

--- sh/base.sh
+++
@@ -1,2 +0,0 @@
-alias openmanus='unset all_proxy && unset ALL_PROXY && cd /home/yunlk/soft/dev/apps/OpenManus && uv run main.py'
-alias openmanus_sandbox='unset all_proxy && unset ALL_PROXY && cd /home/yunlk/soft/dev/apps/OpenManus && uv run sandbox_main.py'
